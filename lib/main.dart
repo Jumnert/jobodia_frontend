49f8b900e485167cdaa2362dd64c25ead39445ed
Ranoscyber <sonayaya92@gmail.com>
2026-06-01 18:26:28 +0700

    cleaned in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,14 +9,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-    home: Scaffold(
-      body: Center(
-        child: const Text("Hello"),
-      ),
-    ),
+    return const MaterialApp(
+      home: Scaffold(body: Center(child: Text("Hello"))),
     );
   }
 }
-
-
